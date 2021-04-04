a293bba1122c80bf01cb6e8fb057d77334f3744b
Giovanni Gerratana <giovannigerratana1@gmail.com>
2021-04-05 00:19:43 +0200

    modifiche a script per aggiunta Label, modifiche nomi a file json e manifest, file Readme aggiornato, divisione dei manifest di output

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Bioinfo_Project
+
+
+1) Script#1
+
+- Aggiungere i seguenti parametri per eseguire lo script di creazione del manifest per il download dei dati:
+
+C:/Users/Giovanni/Desktop/Bio_Project/unione_maninfest/1_miRNA.json
+C:/Users/Giovanni/Desktop/Bio_Project/unione_maninfest/2_mRNA.json
+C:/Users/Giovanni/Desktop/Bio_Project/unione_maninfest/1_miRNA_manifest.txt
+C:/Users/Giovanni/Desktop/Bio_Project/unione_maninfest/2_mRNA_manifest.txt
+
+- Mettere come working directory la cartella 'unione_maninfest'.
+
+- Avviare script.
--- unione_maninfest/extractManifest.py
+++ unione_maninfest/extractManifest.py
@@ -1,84 +1,100 @@
 # coding=utf-8
 from sys import argv
+import os
 import json
 
+
 def saveOccurences(path, flag):
-	id = ''
-	fname = ''	
-	with open(path) as f:
-		line = f.readline()
-		while line:
-			if "case_id" in line:
-				id = line.split(' ')[7].split('"')[1]
-			if "file_name" in line:
-				fname = line.split(' ')[3].split('"')[1]
+    id = ''
+    fname = ''
+    proj_id = ''
+    with open(path) as f:
+        line = f.readline()
+        while line:
+            if "case_id" in line:
+                id = line.split(' ')[7].split('"')[1]
+            if "project_id" in line:
+                proj_id = line.split(' ')[9].split('"')[1]
+            if "file_name" in line:
+                fname = line.split(' ')[3].split('"')[1]
 
-				# se ho una coppia non nulla di (id-fname) allora la aggiungo al dizionario
-				if  id!='' and fname!='':
-					# se id non è ancora presente, lo aggiungo con un value nullo
-					if id not in occ.keys():
-						occ[id] = []		
-					occ[id].append(fname)		# altrimenti aggiungo (appendo) solo il valore di fname
-					id = ''
-					fname = ''
-			line = f.readline()
-		f.close()
+                # se ho una coppia non nulla di (id-fname) allora la aggiungo al dizionario
+                if id != '' and fname != '':
 
+                    # 1) aggiungo al file label corrispondente fname e project_id
+                    if flag == 0:   # stiamo leggendo il file mi_RNA
+                        label_miRNA.write(fname + ':' + "['" + proj_id + "']" + '\n')
+                    else:  # file m_RNA
+                        label_mRNA.write(fname + ':' + "['" + proj_id + "']" + '\n')
 
-	if flag == 1:	# vuol dire che sto lavorando sul secondo file: tolgo occorrenze singole 
-					#e mantengo quelle in cui ho almeno un file di mrna e uno di mirna
-		#print("Deleting...")
-		
-		with open("deleted.txt", 'w') as deleted:
-			for key in list(occ.keys()):
-				if len(occ[key]) < 2:
-					deleted.write(str(key) + '\n')
-					deleted.write('\t\t' + ' --> '+ str(occ[key]) + '\n')
-					occ.pop(key)
-		deleted.close()
+                    # 2) aggiorno dizionario: se id non è ancora presente, lo aggiungo con un value nullo
+                    if id not in occ.keys():
+                        occ[id] = []
+                    occ[id].append(fname)  # altrimenti aggiungo (appendo) solo il valore di fname
+                    id = ''
+                    fname = ''
+                    proj_id = ''
+            line = f.readline()
+        f.close()
 
-		with open("dump.txt", 'w') as dump:
-			n=0
-			for k in occ.keys():
-				dump.write(str(k) + '\t(' + str( len(occ[k]) ) + ')' +'\n')
-				for v in occ[k]:
-					n=n+1
-					dump.write('\t\t' + ' --> '+ str(v) + '\n')
-			dump.write('totale chiavi :' + str(len(occ)) + '\n' )
-			dump.write('totale file :' + str(n) + '\n' )
-			dump.write('\n\n\n')
-			
-		dump.close()
+    if flag == 1:  # vuol dire che sto lavorando sul secondo file: tolgo occorrenze singole
+        # e mantengo quelle in cui ho almeno un file di mrna e uno di mirna
+        # print("Deleting...")
 
+        with open("deleted.txt", 'w') as deleted:
+            for key in list(occ.keys()):
+                if len(occ[key]) < 2:
+                    deleted.write(str(key) + '\n')
+                    deleted.write('\t\t' + ' --> ' + str(occ[key]) + '\n')
+                    occ.pop(key)
+        deleted.close()
+
+        with open("dump.txt", 'w') as dump:
+            n = 0
+            for k in occ.keys():
+                dump.write(str(k) + '\t(' + str(len(occ[k])) + ')' + '\n')
+                for v in occ[k]:
+                    n = n + 1
+                    dump.write('\t\t' + ' --> ' + str(v) + '\n')
+            dump.write('totale chiavi :' + str(len(occ)) + '\n')
+            dump.write('totale file :' + str(n) + '\n')
+            dump.write('\n\n\n')
+
+        dump.close()
 
 
 def getManifestRecord(path):
-	with open(path,'r') as f:
-		#manifestList = []
-		manifestLines = f.readlines()
-		if manifestLines[0] not in manifestList:
-			manifestList.append(manifestLines[0])
+    with open(path, 'r') as f:
+        # manifestList = []
+        manifestLines = f.readlines()
+        if manifestLines[0] not in manifestList:
+            manifestList.append(manifestLines[0])
 
-		kidneyFiles=[]
-		for k in occ.keys():
-			for v in occ[k]:
-				kidneyFiles.append(v)
-		
-		#per ogni riga del file manifest cerco quelle che hanno come filename 
-		#una corrispondenza con i values del dizionario
-		for l in manifestLines:
-			record = l.split('\t')
-			if record[1] in kidneyFiles:
-				manifestList.append(l)
-		f.close()
+        kidneyFiles = []
+        for k in occ.keys():
+            for v in occ[k]:
+                kidneyFiles.append(v)
+
+        # per ogni riga del file manifest cerco quelle che hanno come filename
+        # una corrispondenza con i values del dizionario
+        for l in manifestLines:
+            record = l.split('\t')
+            if record[1] in kidneyFiles:
+                manifestList.append(l)
+        f.close()
 
 
 print("Main()")
 scriptname, json1, json2, manifest1, manifest2 = argv
 occ = {}
 manifestList = []
+label_mRNA = open('../label_mRNA.txt', 'w+')
+label_miRNA = open('../label_miRNA.txt', 'w+')
 saveOccurences(json1, 0)
 saveOccurences(json2, 1)
+
+label_miRNA.close()
+label_mRNA.close()
 
 # stampo primi record del dizionario
 # i=0
@@ -92,9 +108,14 @@ saveOccurences(json2, 1)
 getManifestRecord(manifest1)
 getManifestRecord(manifest2)
 
-#print(manifestList)
-#Scrivo il nuovo manifest
-fout = open('kidney_manifest.txt','w+')
+# print(manifestList)
+# Scrivo il nuovo manifest
+fout_mRNA = open('mRNA_kidney_manifest.txt', 'w+')
+fout_miRNA = open('miRNA_kidney_manifest.txt', 'w+')
 for line in manifestList:
-    fout.write(line)
-fout.close()
+    if 'quantification' in line:
+        fout_miRNA.write(line)
+    else:
+        fout_mRNA.write(line)
+fout_miRNA.close()
+fout_mRNA.close()
