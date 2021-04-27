897bc04372276dd4075aac49941a64d6ea285e60
GiulianoGiu <81929802+GiulianoGiu@users.noreply.github.com>
2021-04-27 18:30:58 +0200

    Add files via upload

--- FS_random_forest_linear_reg.py
+++ FS_random_forest_linear_reg.py
@@ -13,6 +13,7 @@ from matplotlib import pyplot
 #Import data
 data = pd.read_csv('dataset_finale_miRNa.csv', index_col=[0])
 
+
 ## Random Forest mean decrease impurity
 target = []
 X1 = data.values #matrice con solo i valori numerici
@@ -20,14 +21,6 @@ Y1 = list(data.axes[0]) #target dei 'tumori' (tipologia)
 for line in Y1:
     line = line.strip('"(').strip("',)")
     target.append(line) #rimuovo caratteri in eccesso
-target_b = pd.get_dummies(target) # random forest non accetta come input il target sotto forma di caratteri per questo utilizzo pdgetdummies
-names1 = np.array(data.axes[1]) #nomi delle features (geni)
-rf = RandomForestRegressor()
-rf.fit(X1,target_b)
-print( "Random Forest features sorted by their score:")
-print( sorted(zip(map(lambda x: round(x, 4), rf.feature_importances_), names1),reverse=True)) #stampo in ordine le features
-
-## Stability selection
 
 # integer encode
 #Ho bisogno di trasformare il target da caratteri a numeri, nel random forest viene fatto da pd.getdummies per linear regression
@@ -35,12 +28,23 @@ print( sorted(zip(map(lambda x: round(x, 4), rf.feature_importances_), names1),r
 label_encoder = LabelEncoder()
 target_i= label_encoder.fit_transform(target) #target integer encoded
 
+target_b = pd.get_dummies(target) # random forest non accetta come input il target sotto forma di caratteri per questo utilizzo pdgetdummies
+names1 = np.array(data.axes[1]) #nomi delle features (geni)
+rf = RandomForestRegressor()
+rf.fit(X1,target_i)
+print( "Random Forest features sorted by their score:")
+print( sorted(zip(map(lambda x: round(x, 4), rf.feature_importances_), names1),reverse=True)) #stampo in ordine le features
+
+## Stability selection
+
+
+
 ## Linear regression
 from sklearn.linear_model import LinearRegression
 # define the model
 model = LinearRegression()
 # fit the model
-model.fit(data, target_i)
+model.fit(X1, target_i)
 # get importance
 importance = model.coef_
 # summarize feature importance
@@ -51,3 +55,55 @@ print( sorted(zip(map(lambda x: round(x, 4), importance), names1),reverse=True))
 # # plot feature importance
 # pyplot.bar([x for x in range(len(importance))], importance)
 # pyplot.show()
+
+##Logistic Regression (not works)
+# from sklearn.linear_model import LogisticRegression
+# # define the model
+# LG = LogisticRegression()
+# # fit the model
+# LG.fit(X1, target_i)
+# # get importance
+# #importance = model.coef_[0]
+# # summarize feature importance
+# print( "Logistic regression features sorted by their score:")
+# print( sorted(zip(map(lambda x: round(x, 4), LG.coef_[0]), names1),reverse=True))
+
+##Deision tree
+from sklearn.tree import DecisionTreeRegressor
+# define the model
+DCT = DecisionTreeRegressor()
+# fit the model
+DCT.fit(X1, target_i)
+# get importance
+#importance = DCT.feature_importances_
+# summarize feature importance
+print( "Decision tree features sorted by their score:")
+print( sorted(zip(map(lambda x: round(x, 4), DCT.feature_importances_), names1),reverse=True))
+
+#xgboost
+from xgboost import XGBRegressor
+# define the model
+XGB = XGBRegressor()
+# fit the model
+XGB.fit(X1, target_i)
+# get importance
+#importance = model.feature_importances_
+# summarize feature importance
+print( "XGBoost features sorted by their score:")
+print( sorted(zip(map(lambda x: round(x, 4), XGB.feature_importances_), names1),reverse=True))
+
+## KNeighborsRegressor
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.inspection import permutation_importance
+# define the model
+KN = KNeighborsRegressor()
+# fit the model
+KN.fit(X1, target_i)
+# perform permutation importance
+results = permutation_importance(KN, X1, target_i, scoring='neg_mean_squared_error')
+# get importance
+#importance = results.importances_mean
+# summarize feature importance
+print( "KNeighborsRegressor features sorted by their score:")
+print( sorted(zip(map(lambda x: round(x, 4), results.importances_mean), names1),reverse=True))
+
