fb992a70f4c52108d8abbdd84e3803bc2461eb4b
Giovanni Gerratana <giovannigerratana1@gmail.com>
2021-06-08 14:54:16 +0200

    cambio nomi script creazione_manifest

--- n1_unione_maninfest/creazione_manifest.py
+++ n1_unione_maninfest/creazione_manifest.py
@@ -10,26 +10,26 @@ list_m = {}  # case_id : file_name
 label_m = {}  # file_name : project_id
 line_m = f_m_j.readline()
 while line_m:
-    if "case_id" in line_m:
-        alfio = line_m.split(' ')
-        alfio = alfio[7].split('"')
-        alfio = alfio[1]
+    if "case_id" in line_m:  # id paziente
+        caseid = line_m.split(' ')
+        caseid = caseid[7].split('"')
+        caseid = caseid[1]
 
-    if "project_id" in line_m:
+    if "project_id" in line_m:  # tipo di tumore
         lb = line_m.split(' ')
         lb = lb[9].split('"')
         lb = lb[1]
 
     if "file_name" in line_m:
-        saro = line_m.split(' ')
-        saro = saro[3].split('"')
-        saro = saro[1]
+        filename = line_m.split(' ')
+        filename = filename[3].split('"')
+        filename = filename[1]
 
-        list_m[saro] = []
-        list_m[saro].append(alfio)
+        list_m[filename] = []  # dict con (filename,case_id)
+        list_m[filename].append(caseid)
 
-        label_m[saro] = []
-        label_m[saro].append(lb)
+        label_m[filename] = []      # dict con (filename, label tumore)
+        label_m[filename].append(lb)
 
     line_m = f_m_j.readline()
 f_m_j.close()
@@ -41,9 +41,9 @@ label_mi = {}
 list_mi = {}
 while line_mi:
     if "case_id" in line_mi:
-        alfio = line_mi.split(' ')
-        alfio = alfio[7].split('"')
-        alfio = alfio[1]
+        caseid = line_mi.split(' ')
+        caseid = caseid[7].split('"')
+        caseid = caseid[1]
 
     if "project_id" in line_mi:
         lb = line_mi.split(' ')
@@ -51,20 +51,20 @@ while line_mi:
         lb = lb[1]
 
     if "file_name" in line_mi:
-        saro = line_mi.split(' ')
-        saro = saro[3].split('"')
-        saro = saro[1]
+        filename = line_mi.split(' ')
+        filename = filename[3].split('"')
+        filename = filename[1]
 
-        label_mi[saro] = []
-        label_mi[saro].append(lb)
+        label_mi[filename] = []     # dict con (filename, label tumore)
+        label_mi[filename].append(lb)
 
-        list_mi[saro] = []
-        list_mi[saro].append(alfio)
+        list_mi[filename] = []      # dict con (filename,case_id)
+        list_mi[filename].append(caseid)
     line_mi = f_mi_j.readline()
 f_mi_j.close()
 
-man_m = []
-man_mi = []
+man_m = []  # lista di filename:caseId senza doppioni e pazienti presenti in entrambi i file json (per mRNA)
+man_mi = []  # lista di filename:caseId senza doppioni e pazienti presenti in entrambi i file json (per miRNA)
 for k in list(list_m.values()):
     if k in list(list_mi.values()):
         man_m.append(list(list_m.keys())[list(list_m.values()).index(k)])  # get keys from value
@@ -86,39 +86,39 @@ for file in man_mi:
         new_label_mi[file] = []
         new_label_mi[file].append(label_mi[file])
 
-# #Creo il file ciao che sarò il mio nuovo manifest con soltanto i soggetti in comune
+# Creo il file ciao che sarò il mio nuovo manifest con soltanto i soggetti in comune
 f_m_m = open(mrnaManifest, 'r')
-ciao = []
+unionManifest_m = []
 line_m = f_m_m.readlines()
-ciao.append(line_m[0])
+unionManifest_m.append(line_m[0])
 for line in man_m:
     for riga in line_m:
         riga1 = riga.split('\t')
         if riga1[1] == line:
-            ciao.append(riga)
+            unionManifest_m.append(riga)
 f_m_m.close()
 
-# #Creo il file ciao che sarò il mio nuovo manifest con soltanto i soggetti in comune
+# #Creo il file unionManifest che sarò il mio nuovo manifest con soltanto i soggetti in comune
 f_m_m = open(mirnaManifest, 'r')
-ciao1 = []
+unionManifest_mi = []
 line_m = f_m_m.readlines()
-ciao1.append(line_m[0])
+unionManifest_mi.append(line_m[0])
 for line in man_mi:
     for riga in line_m:
         riga1 = riga.split('\t')
         if riga1[1] == line:
-            ciao1.append(riga)
+            unionManifest_mi.append(riga)
 f_m_m.close()
 
 # Salvataggi
 # Manifest mRNA
 f = open('nuovo_manifest_m.txt', 'w+')
-for line in ciao:
+for line in unionManifest_m:
     f.write(line)
 f.close()
 # #Manifest miRNA
 f = open('nuovo_manifest_mi.txt', 'w+')
-for line in ciao1:
+for line in unionManifest_mi:
     f.write(line)
 f.close()
 # Label miRNA
