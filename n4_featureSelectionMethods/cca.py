2d97c10d92707a084f772392fcd4a772120cde3c
Giovanni Gerratana <giovannigerratana1@gmail.com>
2021-06-11 19:14:16 +0200

    modifica parametri CCA

--- n4_featureSelectionMethods/cca.py
+++ n4_featureSelectionMethods/cca.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import numpy as np
-import rcca
+# import rcca
 from sklearn.preprocessing import LabelBinarizer
 from numpy import array
+
 """
 'cca_fs' is a function that selecet the best features of each dataset compute the canonical correlation analysis
 In particulare given the datasets as input, the function computes the score for each features stariting from
@@ -20,9 +21,9 @@ miRNA_ft : list of top n miRNA features
 
 """
 
-##-----------------------CCA
-def cca_fs(m_data,mi_data,n,save,label=None):
 
+##-----------------------CCA
+def cca_fs(m_data, mi_data, n, label, save):
     values_m = m_data.values
     values_mi = mi_data.values
     names_m_ft = np.array(m_data.axes[1])
@@ -31,39 +32,40 @@ def cca_fs(m_data,mi_data,n,save,label=None):
     # cca_cross = rcca.CCACrossValidate(kernelcca=True, ktype='gaussian', regs=[1e-3, 1e-2, 1e-1], numCCs=[2, 10, 20])
     # cca_cross.train([values_m, values_mi])
     # cca_cross.train([values_m, values_mi,label])
-    #To tune hyperparamenters reg and numCC use rcca.crossvalidate to obtain the best values.
-    #If the hyperparameters are already tuned use rcca.CCA
+    # To tune hyperparamenters reg and numCC use rcca.crossvalidate to obtain the best values.
+    # If the hyperparameters are already tuned use rcca.CCA
 
-    #In our case we obtain the best results with reg = 0.1 numCC = 10
-    cca = rcca.CCA(kernelcca=True, reg=0.1, numCC=10)
+    # In our case we obtain the best results with reg = 0.1 numCC = 10
+    cca = rcca.CCA(kernelcca=True, ktype='gaussian', reg=0.1, numCC=10)
 
-    if label is None:
+    if label is False:
         cca.train([values_m, values_mi])
     else:
+        label = list(m_data.axes[0])
         label = array(label)
         # Binary encode
         lb = LabelBinarizer()
-        list = lb.fit_transform(label) #Target transformed in binary encode
-        cca.train([values_m, values_mi,list])
+        list = lb.fit_transform(label)  # Target transformed in binary encode
+        cca.train([values_m, values_mi, list])
 
-    #Canonical weights
+    # Canonical weights
     W1 = cca.ws[0]
     W2 = cca.ws[1]
 
     W1 = pd.DataFrame(W1)
-    #Compute the normalized weights matrix by normalize each value for the absolute value of the sum of the column
+    # Compute the normalized weights matrix by normalize each value for the absolute value of the sum of the column
     Q1 = np.zeros([W1.shape[0], W1.shape[1]])
     Q1 = pd.DataFrame(Q1)
     for i in range(W1.shape[1]):
         for j in range(W1.shape[0]):
             Q1[i][j] = abs(W1[i][j]) / sum(abs(W1[i]))
 
-    #Determine the importance score for each features
+    # Determine the importance score for each features
     score1 = np.zeros([W1.shape[0], 1])
     for i in range(len(score1)):
         score1[i] = sum(Q1.values[i]) / Q1.values.sum()
 
-    #Same procedures for the second weights matrix
+    # Same procedures for the second weights matrix
     W2 = pd.DataFrame(W2)
     Q2 = np.zeros([W2.shape[0], W2.shape[1]])
     Q2 = pd.DataFrame(Q2)
@@ -76,7 +78,7 @@ def cca_fs(m_data,mi_data,n,save,label=None):
     for i in range(len(score2)):
         score2[i] = sum(Q2.values[i]) / Q2.values.sum()
 
-    #Order the most important n features
+    # Order the most important n features
     top_mft = []
     sorted_m_ft = sorted(zip(map(lambda x: x, score1), names_m_ft), reverse=True)
     for i in range(n):
@@ -89,14 +91,18 @@ def cca_fs(m_data,mi_data,n,save,label=None):
         top_mift.append(sorted_mi_ft[i][1])
     top_mi_data = mi_data[top_mift]
 
-    CCA_result_dataset = pd.concat([top_m_data, top_mi_data], axis=1) #new dataset composed by top n mRNA and miRNA ft
-    if save == 1:
-        CCA_result_dataset.to_csv('CCA_data_poly_lin.csv')
+    CCA_result_dataset = pd.concat([top_m_data, top_mi_data], axis=1)  # new dataset composed by top n mRNA and miRNA ft
+    if save is True:
+        CCA_result_dataset.to_csv('CCA_data.csv')
     return CCA_result_dataset, top_mft, top_mift
 
-#Import Data
-m_data = pd.read_csv('best_100_xgb_mRNA_data.csv', index_col=[0])
-mi_data = pd.read_csv('best_100_xgb_miRNA_data.csv', index_col=[0])
-target = list(m_data.axes[0])
-n_ft = 50
-[data, mRNA_ft, miRNA_ft] = cca_fs(m_data,mi_data,n_ft,0,target)
+
+# Import Data
+file1 = input('> Dataset1 = ')
+file2 = input('> Dataset2 = ')
+n_ft = int(input('> Number of feature = '))
+supervised = bool(input('> Supervised version? (1=Yes/0=No)'))
+save = bool(input('> Save CCA dataset? (1=Yes/0=No)'))
+m_data = pd.read_csv(file1, index_col=[0])
+mi_data = pd.read_csv(file2, index_col=[0])
+[data, mRNA_ft, miRNA_ft] = cca_fs(m_data, mi_data, n_ft, supervised, save)
